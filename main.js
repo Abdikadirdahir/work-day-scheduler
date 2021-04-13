ae889ec5f0530560eb4a32a52386aa9f1e3cd445
Abdikadirdahir <61722709+Abdikadirdahir@users.noreply.github.com>
2021-04-13 12:55:56 -0700

    Completed version of the assigment

--- main.js
+++ main.js
@@ -1,65 +1,53 @@
-var getLastDayofMonth = function(currentMonth) {
-    var dt = new Date();
-    dt.setMonth(currentMonth + 1);
-    dt.setDate(0);
-    return dt.getDate();
-};
+/*GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+*/
 
-var today = new Date();
-var thisMonth = today.getMonth();
+var currentDateEl = $("#currentDay");
+var timeblocksEl = $(".time-block");
+var buttonClick = ("button-click")
 
-// display month and year
-$("#month_year").text( getMonthText(thisMonth) + " " + today.getFullYear() );
+function todayDate() {
+    var theDate = moment().format("MMM DD, YYYY");
+    currentDateEl.text("Today date is " + theDate);
+    timeblocksEl.each(function () {
+        var currentHour = moment().hours()
+        if (currentHour === $(this).data().time) {
+            $(this).children("textarea").addClass("present");
 
-var lastDayofMonth = getLastDayofMonth(thisMonth);
-var rows = $("#calendar").html();
-
-var date; 
-var day;  
-var start;
-
-// loop through the number of days in the month
-for (var i = 0; i < lastDayofMonth; i++) {
-    // add one to index and use that number as current date
-    today.setDate(i + 1);
-    
-    // get the current date and day
-    date = today.getDate();
-    day = today.getDay();
-    
-    // start a new row if it's the first of the month or the day is Sunday
-    if (date === 1 || day === 0) { 
-        rows = rows.concat("<tr>"); 
-    }
-
-    // add blank dates at the beginning of the calendar until
-    // you get to the day of the week the month starts on
-    if (date === 1 ) { 
-        start = 0;
-        while (start < day) {
-            rows = rows.concat("<td></td>"); 
-            start++;
+        } else if (currentHour > $(this).data().time) {
+            $(this).children("textarea").addClass("past");
+        } else {
+            $(this).children("textarea").addClass("future");
         }
-    } 
-    
-    // add the date to the calendar
-    rows = rows.concat("<td>", date, "</td>");
-    
-    // add blank dates at the end of the calendar until
-    // you get to the last day of the week the month ends in
-    if (date === lastDayofMonth) { 
-        start = day;
-        while (start < 6) {
-            rows = rows.concat("<td></td>"); 
-            start++;
-        }
-    } 
-    
-    // end the row if it's the last of the month or the day is Saturday
-    if (date === lastDayofMonth || day === 6) { 
-        rows = rows.concat("</tr>"); 
-    }
+    })
 }
 
-// display calendar rows
-$("#calendar").html(rows);
+todayDate();
+
+setInterval(todayDate, 10000);
+
+
+$(".saveBtn").on("click", function () {
+    var saveTime = $(this).siblings("textarea").val();
+    var whatTime = $(this).parent(".time-block").data("time");
+    console.log(whatTime)
+    localStorage.setItem(whatTime, saveTime)
+
+})
+
+$("textarea").each(function () {
+    var getTime = $(this).parent(".time-block").data("time");
+    $(this).val(localStorage.getItem(getTime));
+})
+
